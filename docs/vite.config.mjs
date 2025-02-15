936c2a5e742f25b4aaed2b077ad11edc2e2b6382
PiotrOtta <ottapiotr@gmail.com>
2025-02-15 19:42:50 +0100

    fix: :green_heart: build fixes

--- docs/vite.config.mjs
+++ docs/vite.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '',
+  base: './',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
