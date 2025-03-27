7296376b6b0560c692ed39b619fb77cbacbf7ab2
PiotrOtta <ottapiotr@gmail.com>
2025-03-27 18:52:33 +0100

    build: :construction_worker: added react compiler plugin + version in package-lock

--- docs/eslint.config.js
+++ docs/eslint.config.js
@@ -1,10 +1,18 @@
 import mantine from 'eslint-config-mantine';
+import reactCompiler from 'eslint-plugin-react-compiler';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(...mantine, {
-  ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'],
-  rules: {
-    curly: 'off',
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-  },
-});
+export default tseslint.config(
+  { ...mantine[0], ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts', 'dist'] },
+  {
+    ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts', 'dist'],
+    plugins: {
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      curly: 'off',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'react-compiler/react-compiler': 'error',
+    },
+  }
+);
--- docs/vite.config.mjs
+++ docs/vite.config.mjs
@@ -2,9 +2,24 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const ReactCompilerSettings = {
+  target: '19',
+  sources: (filename) => {
+    // if DocsSpecificTags is handled by the compiler, the _Blockquote select (left panel) won't work
+    return !filename.includes('DocsSpecificTags');
+  },
+};
+
 export default defineConfig({
   base: './',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerSettings]],
+      },
+    }),
+    tsconfigPaths(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
